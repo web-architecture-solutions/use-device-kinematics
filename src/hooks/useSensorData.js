abfc6136933844e938482cebcc3c775380ebb72b
Ariel Pettyjohn <89058021+ariel-pettyjohn@users.noreply.github.com>
2025-01-07 04:18:07 +0000

    Clean up output

--- src/hooks/useSensorData.js
+++ src/hooks/useSensorData.js
@@ -22,30 +22,39 @@ export default function useSensorData(config = {}) {
     await Promise.all([motion.startListening(), orientation.startListening(), geolocation.startListening()])
   }, [motion.startListening, orientation.startListening, geolocation.startListening])
 
+  const data = {
+    linearAcceleration: motion.data?.acceleration,
+    linearAccelerationIncludingGravity: motion.data?.accelerationIncludingGravity,
+    angularVelocity: motion.data?.rotationRate,
+    motionInterval: motion.data?.interval,
+    geolocationTimestamp: geolocation.data?.timestamp
+  }
+
+  if (geolocation.data) {
+    data.position = {
+      latitude: geolocation.data?.latitude,
+      longitude: geolocation.data?.longitude,
+      altitude: geolocation.data?.altitude,
+      accuracy: geolocation.data?.accuracy,
+      altitudeAccuracy: geolocation.data?.altitudeAccuracy
+    }
+
+    data.velocity = {
+      heading: geolocation.data?.heading,
+      speed: geolocation.data?.speed
+    }
+  }
+
+  if (orientation.data) {
+    data.orientation = {
+      yaw: orientation.data.alpha,
+      pitch: orientation.data.beta,
+      roll: orientation.data.gamma
+    }
+  }
+
   return {
-    data: {
-      linearAcceleration: motion.data?.acceleration,
-      linearAccelerationIncludingGravity: motion.data?.accelerationIncludingGravity,
-      angularVelocity: motion.data?.rotationRate,
-      motionInterval: motion.data?.interval,
-      orientation: {
-        yaw: orientation.data?.alpha,
-        pitch: orientation.data?.beta,
-        roll: orientation.data?.gamma
-      },
-      position: {
-        latitude: geolocation.data?.latitude,
-        longitude: geolocation.data?.longitude,
-        altitude: geolocation.data?.altitude,
-        accuracy: geolocation.data?.accuracy,
-        altitudeAccuracy: geolocation.data?.altitudeAccuracy
-      },
-      velocity: {
-        heading: geolocation.data?.heading,
-        speed: geolocation.data?.speed
-      },
-      geolocationTimestamp: geolocation.data?.timestamp
-    },
+    data,
     errors,
     isListening,
     startListening
