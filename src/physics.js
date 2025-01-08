d017cca9e4c24268810d2da96ef6c9e50205fc12
Ariel Pettyjohn <89058021+ariel-pettyjohn@users.noreply.github.com>
2025-01-08 11:24:21 +0000

    Add component physics helpers

--- src/physics.js
+++ src/physics.js
@@ -14,7 +14,11 @@ export function calculateHaversineDistance(p1, p2) {
     Math.sin(deltaLat / 2) * Math.sin(deltaLat / 2) + Math.cos(lat1) * Math.cos(lat2) * Math.sin(deltaLon / 2) * Math.sin(deltaLon / 2)
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
 
-  return R * c
+  return {
+    distance: c * R,
+    deltaLat: deltaLat * R,
+    deltaLon: deltaLon * R
+  }
 }
 
 export function calculateTotalVelocity(p1, p2, timeInterval) {
@@ -40,4 +44,48 @@ export function calculateTotalCurvatureFromAcceleration(totalAcceleration, total
 
 export function calculateTotalCurvatureFromAngularVelocity(totalAngularVelocity, totalVelocity) {
   return totalAngularVelocity / totalVelocity
+}
+
+export function calculateVelocityComponents(p1, p2, timeInterval) {
+  const { deltaLat, deltaLon } = calculateHaversineDistance(p1, p2)
+
+  const xVelocity = deltaLon / timeInterval
+  const yVelocity = deltaLat / timeInterval
+
+  return { xVelocity, yVelocity }
+}
+
+export function calculateAccelerationComponents(velocity1, velocity2, timeInterval) {
+  const xAcceleration = (velocity2.xVelocity - velocity1.xVelocity) / timeInterval
+  const yAcceleration = (velocity2.yVelocity - velocity1.yVelocity) / timeInterval
+
+  return { xAcceleration, yAcceleration }
+}
+
+export function calculateJerkComponents(acceleration1, acceleration2, timeInterval) {
+  const xJerk = (acceleration2.xAcceleration - acceleration1.xAcceleration) / timeInterval
+  const yJerk = (acceleration2.yAcceleration - acceleration1.yAcceleration) / timeInterval
+
+  return { xJerk, yJerk }
+}
+
+export function calculateAngularVelocityComponents(alpha, beta) {
+  const xAngularVelocity = alpha * (Math.PI / 180) // Convert to radians
+  const yAngularVelocity = beta * (Math.PI / 180) // Convert to radians
+
+  return { xAngularVelocity, yAngularVelocity }
+}
+
+export function calculateCurvatureComponentsFromAcceleration(acceleration, velocity) {
+  const xCurvature = acceleration.xAcceleration / Math.pow(velocity.xVelocity, 2)
+  const yCurvature = acceleration.yAcceleration / Math.pow(velocity.yVelocity, 2)
+
+  return { xCurvature, yCurvature }
+}
+
+export function calculateCurvatureComponentsFromAngularVelocity(angularVelocity, velocity) {
+  const xCurvature = angularVelocity.xAngularVelocity / velocity.xVelocity
+  const yCurvature = angularVelocity.yAngularVelocity / velocity.yVelocity
+
+  return { xCurvature, yCurvature }
 }
