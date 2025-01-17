8946862910bd2bdd1e0671ff72aff9174f0f7485
Ariel Pettyjohn <89058021+ariel-pettyjohn@users.noreply.github.com>
2025-01-17 06:23:33 +0000

    Fix velocity calculation bug

--- src/lib/physics.js
+++ src/lib/physics.js
@@ -1,31 +1,47 @@
 import { toRadians } from './math'
 
-export function haversineDistance({ x, y, z }, { x: previousX, y: previousY, z: previousZ }) {
-  const coordinates2D = [x, previousX, y, previousY]
-  if (coordinates2D.some((coordinate) => coordinate === null)) {
-    return { x: null, y: null, z: null, xy: null, xyz: null }
-  }
+export function calculateGeodeticDisplacement(currentPosition, previousPosition) {
+  const { x: previousLongitude, y: previousLatitude, z: previousAltitude } = previousPosition
+  const { x: currentLongitude, y: currentLatitude, z: currentAltitude } = currentPosition
 
-  const R = 6371000
+  const earthRadius = 6371000
 
-  const deltaX = toRadians(x - previousX)
-  const deltaY = toRadians(y - previousY)
-  const deltaZ = z - previousZ
+  const previousLatitudeRadians = toRadians(previousLatitude)
+  const previousLongitudeRadians = toRadians(previousLongitude)
+  const currentLatitudeRadians = toRadians(currentLatitude)
+  const currentLongitudeRadians = toRadians(currentLongitude)
 
-  const lat1 = toRadians(previousY)
-  const lat2 = toRadians(y)
+  const longitudeDifferenceRadians = currentLongitudeRadians - previousLongitudeRadians
 
-  const a = Math.cos(lat1) * Math.cos(lat2) * Math.sin(deltaX / 2) * Math.sin(deltaX / 2)
-  const b = Math.sin(deltaY / 2) * Math.sin(deltaY / 2) + a
-  const c = 2 * Math.atan2(Math.sqrt(b), Math.sqrt(1 - b))
-
-  const xy = c * R
+  const eastwardDisplacement = earthRadius * Math.cos(currentLatitudeRadians) * longitudeDifferenceRadians
+  const northwardDisplacement = earthRadius * (currentLatitudeRadians - previousLatitudeRadians)
+  const verticalDisplacement = currentAltitude - previousAltitude
 
   return {
-    x: deltaX * R * Math.cos((lat1 + lat2) / 2),
-    y: deltaY * R,
-    z: deltaZ,
-    xy: c * R,
-    xyz: Math.sqrt(xy * xy + z * z)
+    x: eastwardDisplacement,
+    y: northwardDisplacement,
+    z: verticalDisplacement
   }
+}
+
+export function calculateHaversineDistance(previousPosition, currentPosition) {
+  const { x: previousLongitude, y: previousLatitude } = previousPosition
+  const { x: currentLongitude, y: currentLatitude } = currentPosition
+
+  const earthRadius = 6371000
+
+  const previousLatitudeRadians = toRadians(previousLatitude)
+  const currentLatitudeRadians = toRadians(currentLatitude)
+  const latitudeDifferenceRadians = toRadians(currentLatitude - previousLatitude)
+  const longitudeDifferenceRadians = toRadians(currentLongitude - previousLongitude)
+
+  const a =
+    Math.sin(latitudeDifferenceRadians / 2) * Math.sin(latitudeDifferenceRadians / 2) +
+    Math.cos(previousLatitudeRadians) *
+      Math.cos(currentLatitudeRadians) *
+      Math.sin(longitudeDifferenceRadians / 2) *
+      Math.sin(longitudeDifferenceRadians / 2)
+  const centralAngle = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+
+  return earthRadius * centralAngle
 }
--- src/variables/Position.js
+++ src/variables/Position.js
@@ -4,44 +4,7 @@ import Velocity from './Velocity'
 
 import { VariableNames } from '../constants'
 
-class GeodeticDisplacement {
-  static R = 6371000
-
-  constructor(currentPosition, previousPosition) {
-    this.currentPosition = currentPosition
-    this.previousPosition = previousPosition
-  }
-
-  get x() {
-    const { x: lon2, y: lat2 } = this.currentPosition
-    const { x: lon1 } = this.previousPosition
-
-    const radLon1 = toRadians(lon1)
-    const radLat2 = toRadians(lat2)
-    const radLon2 = toRadians(lon2)
-
-    const deltaLon = radLon2 - radLon1
-
-    return GeodeticDisplacement.R * Math.cos(radLat2) * deltaLon
-  }
-
-  get y() {
-    const { y: lat2 } = this.currentPosition
-    const { y: lat1 } = this.previousPosition
-
-    const radLat1 = toRadians(lat1)
-    const radLat2 = toRadians(lat2)
-
-    return GeodeticDisplacement.R * (radLat2 - radLat1)
-  }
-
-  get z() {
-    const { z: alt2 } = this.currentPosition
-    const { z: alt1 } = this.previousPosition
-
-    return alt2 - alt1
-  }
-}
+import { calculateGeodeticDisplacement } from '../lib/physics'
 
 export default class Position extends Variable {
   static name = VariableNames.POSITION
@@ -49,11 +12,15 @@ export default class Position extends Variable {
   static initial = { latitude: null, longitude: null, altitude: null }
   static renameComponents = { latitude: 'y', longitude: 'x', altitude: 'z' }
 
-  static componentDerivativeFactory(previous, deltaT) {
-    return ([name, value]) => {
-      const geodeticDisplacement = new GeodeticDisplacement()
-      const delta = value - previous[name]
+  static initializeDerivative(current, previous, deltaT, derivativeConstructor, previousDerivativesWrtT, sensorData) {
+    const geodeticDisplacement = calculateGeodeticDisplacement(current, previous)
+    const initializeComponentDerivative = ([name]) => {
+      const delta = geodeticDisplacement[name]
       return [name, delta / deltaT]
     }
+    const derivativeWrtT = current.map(initializeComponentDerivative)
+    return derivativeConstructor
+      ? new derivativeConstructor(derivativeWrtT, previousDerivativesWrtT, {}, derivativeConstructor, sensorData)
+      : {}
   }
 }
