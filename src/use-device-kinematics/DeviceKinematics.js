ed078ebe2acf3766ef00732a94719bbfa7d756e9
Ariel Pettyjohn <89058021+ariel-pettyjohn@users.noreply.github.com>
2025-01-17 01:24:13 +0000

    Move Vector3 to its own class

--- src/use-device-kinematics/DeviceKinematics.js
+++ src/use-device-kinematics/DeviceKinematics.js
@@ -1,46 +1,5 @@
 import { Matrix } from '../lib/math'
 
-class Vector3 {
-  constructor(x, y, z) {
-    this.x = x
-    this.y = y
-    this.z = z
-  }
-
-  cross(v) {
-    return new Vector3(this.y * v.z - this.z * v.y, this.z * v.x - this.x * v.z, this.x * v.y - this.y * v.x)
-  }
-
-  scale(scalar) {
-    return new Vector3(this.x * scalar, this.y * scalar, this.z * scalar)
-  }
-
-  add(v) {
-    return new Vector3(this.x + v.x, this.y + v.y, this.z + v.z)
-  }
-
-  subtract(v) {
-    return new Vector3(this.x - v.x, this.y - v.y, this.z - v.z)
-  }
-
-  dot(v) {
-    return this.x * v.x + this.y * v.y + this.z * v.z
-  }
-
-  magnitude() {
-    return Math.sqrt(this.x ** 2 + this.y ** 2 + this.z ** 2)
-  }
-
-  normalize() {
-    const mag = this.magnitude()
-    return mag === 0 ? new Vector3(0, 0, 0) : this.scale(1 / mag)
-  }
-
-  toArray() {
-    return [this.x, this.y, this.z]
-  }
-}
-
 export default class DeviceKinematics {
   dimension = 3
 
