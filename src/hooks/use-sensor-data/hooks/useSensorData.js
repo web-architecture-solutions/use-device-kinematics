4bd48d174abe0883ebc67799192197933f2b6a34
Ariel Pettyjohn <89058021+ariel-pettyjohn@users.noreply.github.com>
2025-01-22 04:01:13 +0000

    Componentize IIR filter

--- src/hooks/use-sensor-data/hooks/useSensorData.js
+++ src/hooks/use-sensor-data/hooks/useSensorData.js
@@ -1,57 +1,10 @@
-import { useState, useEffect } from 'react'
-
-import useClock from './useClock'
-
-import { toRadians } from '../../../lib/math'
-
-import SensorData from '../lib/SensorData'
 import useRawSensorData from '../../use-raw-sensor-data'
+import useIIRFilter from './useIIRFilter'
 
 export default function useSensorData(config = {}) {
-  const [sensorData, setSensorData] = useState(SensorData.initial)
-  const [startClock, setStartClock] = useState(false)
-
-  const { timestamp } = useClock(4, startClock)
   const { refreshRates, rawSensorData, errors, isListening, startListening } = useRawSensorData(config)
 
-  useEffect(() => {
-    if (isListening) {
-      if (!startClock) setStartClock(true)
-      setSensorData(
-        (previousSensorData) =>
-          new SensorData(
-            {
-              position: {
-                x: rawSensorData.longitude,
-                y: rawSensorData.latitude,
-                z: rawSensorData.altitude,
-                timestamp: timestamp ?? null
-              },
-              acceleration: {
-                x: rawSensorData.acceleration.x,
-                y: rawSensorData.acceleration.y,
-                z: rawSensorData.acceleration.z,
-                timestamp: timestamp ?? null
-              },
-              orientation: {
-                x: toRadians(rawSensorData.beta),
-                y: toRadians(rawSensorData.gamma),
-                z: toRadians(rawSensorData.alpha),
-                timestamp: timestamp ?? null
-              },
-              angularVelocity: {
-                x: toRadians(rawSensorData.rotationRate.beta),
-                y: toRadians(rawSensorData.rotationRate.gamma),
-                z: toRadians(rawSensorData.rotationRate.alpha),
-                timestamp: timestamp ?? null
-              }
-            },
-            previousSensorData,
-            timestamp
-          )
-      )
-    }
-  }, [rawSensorData, timestamp])
+  const sensorData = useIIRFilter(rawSensorData, isListening)
 
   return {
     sensorData,
